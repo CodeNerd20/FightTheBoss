8ebf7ef55593fbf1e8cef7a639d116ec0f9882ce
CodeNerd20 <146860718+CodeNerd20@users.noreply.github.com>
2024-05-30 20:54:55 -0700

    Build #4 and made fixes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,10 +8,14 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     private int score;
+    public int scoreToGet;
     public TextMeshProUGUI scoreText;
 
     public TextMeshProUGUI gameOverText;
     public Button restartButton;
+
+    public TextMeshProUGUI youWinText;
+    public Button playAgainButton;
 
     public bool isGameActive;
 
@@ -21,14 +25,19 @@ public class GameManager : MonoBehaviour
         score = 0;
         UpdateScore(0);
         isGameActive = true;
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.R) && isGameActive == false)
+        if(Input.GetKeyDown(KeyCode.E) && isGameActive == false)
         {
             RestartGame();
+        }
+        if(score >= scoreToGet)
+        {
+            EnoughPoints();
         }
     }
 
@@ -36,6 +45,14 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+    public void EnoughPoints()
+    {
+        youWinText.gameObject.SetActive(true);
+        isGameActive = false;
+        playAgainButton.gameObject.SetActive(true);
+        Time.timeScale = 0;
     }
 
     public void GameOver()
